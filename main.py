94689c456befaa6300f85e3aa89cc7b177b1bd17
frezario <91615650+frezario@users.noreply.github.com>
2022-02-19 23:40:37 +0200

    Added main functionality

---
+++ main.py
@@ -0,0 +1,96 @@
+'''
+    A project that allows you to approximate cos(3x)^2 using Teylor series.
+'''
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def approximate_value(arg: float, terms_count: int = 50, is_radians: bool = True) -> float:
+    """
+        A function that computes the value of cos(3x)^2
+        using Teylor series.
+    Args:
+        arg (float): an angle (given in radians).
+        terms_count (int): a count of terms in Teylor series. Defaults to 50.
+        is_radians (bool): True if argument is given in radians
+        and False otherwise.
+    Returns:
+        float: a value cos(3*arg)^2
+    Example:
+        >>> approximate_value(99 * math.pi/3, 50)
+        1.0000000000004259
+    """
+    result = 0
+    arg %= 2 * math.pi
+    arg *= 3
+    for num in range(0, terms_count, 2):
+        result += ((-1)**(num/2)) * (arg ** num) / math.factorial(num)
+    return result ** 2
+
+
+def value_difference(arg: float, terms_count=50):
+    """
+    A function that returns difference between approximate value of cos(3*arg)^2
+    and a true value of it.
+    Args:
+        arg (float): an angle (given in radians).
+        terms_count (int, optional): a count of terms in Teylor series. Defaults to 50.
+    Returns:
+        float: a difference.
+    Example:
+        >>> value_difference(math.pi/100)
+        2.220446049250313e-16
+    """
+    return abs(approximate_value(arg, terms_count) - math.cos(3*arg)**2)
+
+
+def plot_graph_difference(terms_count=60):
+    """
+        Draws a curve of a true cos(3x)^2 and it's approximation. Returns nothing.
+    Args:
+        terms_count (int, optional): a count of terms in Teylor series. Defaults to 50. Defaults to 60.
+    """
+    x = np.linspace(-np.pi*10, np.pi*10, 1000)
+    y = np.cos(3 * x) ** 2
+    appr = [approximate_value(point, terms_count) for point in x]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    ax.plot((1), (0), ls="", marker=">", ms=10, color="k",
+            transform=ax.get_yaxis_transform(), clip_on=False)
+    ax.plot((0), (1), ls="", marker="^", ms=10, color="k",
+            transform=ax.get_xaxis_transform(), clip_on=False)
+
+    plt.axis([-30, 30, -10, 10])
+    plt.plot(x, y, 'b')
+    plt.plot(x, appr, 'r')
+
+    plt.show()
+
+
+def epsilon_terms_count(args: list, epsilon: float = 10**(-9))->int:
+    """
+    Returns a count of terms of a series needed to minimize difference between
+    a true value and approximate one.
+    Args:
+        args (list): a list of points, to which we will approximate our function. 
+        epsilon (float, optional): a desired difference. Defaults to 10**(-9).
+    Returns:
+        int: a count of terms.
+    Examples:
+        >>> epsilon_terms_count([45, 80])
+        53
+    """
+    terms_count = 1
+    for arg in args:
+        while value_difference(arg, terms_count) >= epsilon:
+            terms_count += 1
+    return terms_count
